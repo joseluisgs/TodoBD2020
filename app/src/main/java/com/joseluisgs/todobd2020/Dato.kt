00bbabb3fff2851fe2cc0a0fdf02162bcabb61b5
joseluisgs <joseluisgonsan@gmail.com>
2020-10-17 00:29:40 +0200

    Modelo Dato

---
+++ app/src/main/java/com/joseluisgs/todobd2020/Dato.kt
@@ -0,0 +1,9 @@
+package com.joseluisgs.todobd2020
+
+/**
+ * Clase Pojo para el modelo
+ * @property description String
+ * @property imgId Int
+ * @constructor
+ */
+class Dato(var description: String, var imgId: Int)
