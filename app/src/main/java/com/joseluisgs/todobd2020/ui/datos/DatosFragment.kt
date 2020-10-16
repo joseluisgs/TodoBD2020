05fdfaed9ac189f2126445dbdad8e3c4c67a5e9c
joseluisgs <joseluisgonsan@gmail.com>
2020-10-17 00:21:27 +0200

    Main Activity

--- app/src/main/java/com/joseluisgs/todobd2020/ui/datos/DatosFragment.kt
+++ app/src/main/java/com/joseluisgs/todobd2020/ui/datos/DatosFragment.kt
@@ -1,31 +1,27 @@
-package com.joseluisgs.todobd2020
+package com.joseluisgs.todobd2020.ui.datos
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import com.joseluisgs.todobd2020.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class DatosFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_datos, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
