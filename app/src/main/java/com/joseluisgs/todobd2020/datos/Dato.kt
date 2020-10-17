c5411494ab3c68c96a2ed7942a5e72f612e87952
joseluisgs <joseluisgonsan@gmail.com>
2020-10-17 12:31:23 +0200

    Dato Model

--- app/src/main/java/com/joseluisgs/todobd2020/datos/Dato.kt
+++ app/src/main/java/com/joseluisgs/todobd2020/datos/Dato.kt
@@ -1,9 +1,16 @@
 package com.joseluisgs.todobd2020.datos
 
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
 /**
  * Clase Pojo para el modelo
  * @property descripcion String
  * @property imgId Int
  * @constructor
  */
-class Dato(var descripcion: String, var imgId: Int)
+
+// Podemos usar la notacion
+@RealmClass
+open class Dato(@PrimaryKey var id: String, var descripcion: String, var imgId: Int) : RealmObject()
