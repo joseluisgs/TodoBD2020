c8b599f38aac94f39a92e3ba1189b9d7587eb8bf
joseluisgs <joseluisgonsan@gmail.com>
2020-10-17 09:59:12 +0200

    CRUD: Select

--- app/src/main/java/com/joseluisgs/todobd2020/datos/DatosController.kt
+++ app/src/main/java/com/joseluisgs/todobd2020/datos/DatosController.kt
@@ -1,7 +1,17 @@
 package com.joseluisgs.todobd2020.datos
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+
+
 object DatosController {
-    // Inicia la lista de datos
+    // Variables de
+    private const val DATOS_BD = "DATOS_BD_LITE"
+    private const val DATOS_BD_VERSION = 1
+
+    // Inicia la lista de datos con datos ficticios
     fun initDatos(): MutableList<Dato> {
         var datos = mutableListOf<Dato>() // Lista
         datos.add(Dato("Email", android.R.drawable.ic_dialog_email))
@@ -12,4 +22,42 @@ object DatosController {
         datos.add(Dato("Map", android.R.drawable.ic_dialog_map))
         return datos;
     }
+
+    @SuppressLint("Recycle")
+
+    fun getDatos(context: Context, filtro: String?): MutableList<Dato>? {
+        // Abrimos la BD en Modo Lectura
+        val lista = mutableListOf<Dato>()
+        val bdDatos = DatosBD(context, DATOS_BD, null, DATOS_BD_VERSION)
+        val bd: SQLiteDatabase = bdDatos.readableDatabase
+
+        // Podemos hacer la consulta directamente o parametrizada
+        //Cursor c = bd.rawQuery("SELECT * FROM Lugares " + filtro, null);
+        // http://www.sgoliver.net/blog/bases-de-datos-en-android-iii-consultarrecuperar-registros/
+
+        /* Ejemplo de cada campo de la consulta
+            String table = "table2";
+            String[] columns = {"column1", "column3"};
+            String selection = "column3 =?";
+            String[] selectionArgs = {"apple"};
+            String groupBy = null;
+            String having = null;
+            String orderBy = "column3 DESC";
+            String limit = "10";
+
+            Cursor cursor = db.query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+     */
+        val c: Cursor = bd.query(DatosBD.DATOS_TABLE, null, null, null, null, null, filtro, null)
+        if (c.moveToFirst()) {
+            do {
+                val aux = Dato(c.getString(0), c.getInt(1))
+                lista.add(aux)
+            } while (c.moveToNext())
+        }
+        bd.close()
+        bdDatos.close()
+        return lista
+    }
+
+
 }
--- app/src/main/java/com/joseluisgs/todobd2020/datos/DatosDB.kt
+++ app/src/main/java/com/joseluisgs/todobd2020/datos/DatosDB.kt
@@ -15,10 +15,9 @@ import androidx.annotation.Nullable
 class DatosBD(@Nullable context: Context?, @Nullable name: String?, @Nullable factory: CursorFactory?, version: Int) :
     SQLiteOpenHelper(context, name, factory, version) {
 
-    //Sentencia SQL para crear la tabla de de almacenamiento
-    private val DATOS_DB = "DatosSQLite"
-    private val CREATE_DATOS_DB = "CREATE TABLE $DATOS_DB(description TEXT, imgId INTEGER)"
-    private val DELETE_DATOS_DB = "DROP TABLE IF EXISTS $DATOS_DB"
+    //Sentencia SQL para crear la tabla de de almacenamiento. Debemos crearla todo lo que vaya caquí
+    private val CREATE_DATOS_TABLE = "CREATE TABLE $DATOS_TABLE(description TEXT, imgId INTEGER)"
+    private val DELETE_DATOS_TABLE = "DROP TABLE IF EXISTS $DATOS_TABLE"
 
     /**
      * Crea las bases de datos indicadas
@@ -27,7 +26,7 @@ class DatosBD(@Nullable context: Context?, @Nullable name: String?, @Nullable fa
      */
     override fun onCreate(db: SQLiteDatabase) {
         //Cuando se llame al onCreate se realiza la sentencia
-        db.execSQL(CREATE_DATOS_DB)
+        db.execSQL(CREATE_DATOS_TABLE)
     }
 
     /**
@@ -42,7 +41,12 @@ class DatosBD(@Nullable context: Context?, @Nullable name: String?, @Nullable fa
         //      eliminar la tabla anterior y crearla de nuevo vacía con el nuevo formato.
         //      Sin embargo lo normal será que haya que migrar datos de la tabla antigua
         //      a la nueva, por lo que este método debería ser más elaborado.
-        db.execSQL(DELETE_DATOS_DB)
-        db.execSQL(CREATE_DATOS_DB)
+        db.execSQL(DELETE_DATOS_TABLE)
+        db.execSQL(CREATE_DATOS_TABLE)
+    }
+
+    // Variables de clase
+    companion object {
+        val DATOS_TABLE = "DATOS_TABLE"
     }
 }
